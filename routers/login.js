2b5cab8b98d9665247d31dbbed47798ec64a11c0
Mohamed Khaled <wekenzi1995@gmail.com>
2020-04-08 22:03:09 +0200

    lol

--- routers/login.js
+++ routers/login.js
@@ -4,33 +4,32 @@ const clientModel = require('../models/client');
 const jwt = require('jsonwebtoken');
 
 login.post('/',(req,res,next)=>{
-  return res.status(200).json({'welcome':'welcome'})
-  // clientModel.find({email:req.body.email})
-  // .then((data)=>{
-  //   if(data.length < 1 || data[0].password != req.body.password){
-  //     return res.status(401).json({
-  //       message:'Check email and password'
-  //     })
-  //   }else{
-  //     if(data[0].valid != null){
-  //       return res.status(406).json({
-  //         message:'please verify your account'
-  //       })
-  //     }else{
-  //       let token = jwt.sign(
-  //         {email:data[0].email,clientId:data[0]._id},
-  //         'air_port@123',
-  //         // {
-  //         //   expiresIn:86400
-  //         // }
-  //       );
-  //       return res.status(200).json({
-  //         token: token,
-  //         // expiresIn:86400
-  //       })
-  //     }
-  //   }
-  // }).catch(next);
+  clientModel.find({email:req.body.email})
+  .then((data)=>{
+    if(data.length < 1 || data[0].password != req.body.password){
+      return res.status(401).json({
+        message:'Check email and password'
+      })
+    }else{
+      if(data[0].valid != null){
+        return res.status(406).json({
+          message:'please verify your account'
+        })
+      }else{
+        let token = jwt.sign(
+          {email:data[0].email,clientId:data[0]._id},
+          'air_port@123',
+          // {
+          //   expiresIn:86400
+          // }
+        );
+        return res.status(200).json({
+          token: token,
+          // expiresIn:86400
+        })
+      }
+    }
+  }).catch(next);
 });
 
 
