828dc400b9ff59bcbf7588ab3cc507baea0a2cf9
Mohamed Khaled <wekenzi1995@gmail.com>
2020-04-09 20:17:48 +0200

    last

--- routers/register.js
+++ routers/register.js
@@ -16,38 +16,40 @@ register.post('/',(req,res,next)=>{
         // Create Verify Token
         let email = data.email;
         let verifyToken = data.valid;
-        
 
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-          // host: "smtp-mail.outlook.com",
-          service: "hotmail",
-          // secureConnection: false, // TLS requires secureConnection to be false
-          // port: 587, // port for secure SMTP
-          // tls: {
-          //   rejectUnauthorized:false
-          // },
-          auth: {
-              user: 'handsome.boy.195@hotmail.com',
-              pass: 'nightlight'
+        async function sendEmail (){
+          // create reusable transporter object using the default SMTP transport
+          let transporter = nodemailer.createTransport({
+            // host: "smtp-mail.outlook.com",
+            service: "hotmail",
+            // secureConnection: false, // TLS requires secureConnection to be false
+            // port: 587, // port for secure SMTP
+            // tls: {
+            //   rejectUnauthorized:false
+            // },
+            auth: {
+                user: 'handsome.boy.195@hotmail.com',
+                pass: 'nightlight'
+            }
+          });
+
+          let mailOptions = {
+            from: '"From app" <handsome.boy.195@hotmail.com>',
+            to: email,
+            subject: "Verify Your Account",
+            html: `
+            <h2>Welcome to our application</h2>
+            <p>Please click the link below to verify your account</p>
+            <p>
+              <a href="http://localhost:4200/confirm/${verifyToken}/${email}" target="_blank">Verfiy Account</a>
+            </p>
+            ` 
           }
-        });
-
-        let mailOptions = {
-          from: '"From app" <handsome.boy.195@hotmail.com>',
-          to: email,
-          subject: "Verify Your Account",
-          html: `
-          <h2>Welcome to our application</h2>
-          <p>Please click the link below to verify your account</p>
-          <p>
-            <a href="http://localhost:4200/confirm/${verifyToken}/${email}" target="_blank">Verfiy Account</a>
-          </p>
-          ` 
+          // send mail with defined transport object
+          let ifno = await transporter.sendMail(mailOptions);
         }
-      
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions);
+
+        main().catch(console.error);
 
         return res.status(200).json({
           message:'Thanks for register, please verify your email'
