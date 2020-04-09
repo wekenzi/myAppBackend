6f05e314e5fe487f59c2b0a29063bf484aea1d0e
Mohamed Khaled <wekenzi1995@gmail.com>
2020-04-09 20:38:22 +0200

    dsdd

--- routers/register.js
+++ routers/register.js
@@ -22,11 +22,11 @@ register.post('/',(req,res,next)=>{
           let transporter = nodemailer.createTransport({
             // host: "smtp-mail.outlook.com",
             service: "gmail",
-            // secureConnection: false, // TLS requires secureConnection to be false
+            secureConnection: false, // TLS requires secureConnection to be false
             // port: 587, // port for secure SMTP
-            // tls: {
-            //   rejectUnauthorized:false
-            // },
+            tls: {
+              rejectUnauthorized:false
+            },
             auth: {
                 user: 'firstwekeapp@gmail.com',
                 pass: 'nightlight1234'
